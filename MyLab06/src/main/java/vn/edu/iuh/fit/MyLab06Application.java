1061fd15df321ab7d700b881c910dc614b787716
LeMinhThat <mthat456@gmail.com>
2024-11-09 18:29:22 +0700

    Create Project

---
+++ MyLab06/src/main/java/vn/edu/iuh/fit/MyLab06Application.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLab06Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyLab06Application.class, args);
+    }
+
+}
